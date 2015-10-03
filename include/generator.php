8f52e22b2d4e7536c790ebd21097036aa26fda87
Adam Leff <aleff@tests-MacBook-Pro.local>
2015-10-03 15:04:24 -0400

    Add images, begin sketching out generator class

---
+++ include/generator.php
@@ -0,0 +1,24 @@
+<?php
+class Generator{
+    private $height;
+    private $width;
+    private $edges;
+    private $complexity;
+
+    public function init($size,$complexity){
+
+    }
+
+    public function createContinents(){
+
+    }
+
+    public function createIslands(){
+
+    }
+
+    public function createTopography(){
+        
+    }
+}
+?>
