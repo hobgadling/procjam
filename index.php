ec0ca82b12e387f8bff463eafd173c8e2d899bf2
Adam Leff <aleff@tests-MacBook-Pro.local>
2015-10-03 14:49:39 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  include 'include/generator.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ProcJam 2015</title>
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+  <div id="container">
+
+  </div>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</body>
+</html>
